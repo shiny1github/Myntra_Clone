742d2444bfeb17f2bf9315e8ef0303325a8bfb7e
shiny1github <shivanisuman298@gmail.com>
2025-07-25 13:08:38 +0530

    Initial Commit

---
+++ Myntra_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Myntra_Clone
+//
+//  Created by apple on 25/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
